c7d86cc54c4a7659a21ee3f11f7c3403ad9acea1
amirmohammadkz <amirmohammadkz@gmail.com>
2020-06-08 21:11:42 +0430

    Pandora Run preprocessing once

--- data/pandora/author_100recent.py
+++ data/pandora/author_100recent.py
@@ -1,5 +1,5 @@
 from functools import reduce
-
+import os
 import pandas as pd
 import numpy as np
 import multiprocessing as mp
@@ -22,21 +22,26 @@ class MapReducer:
 
 # tmp.groupby('author')["proper_tokenized"].agg(sum)
 def get_100_recent_posts(data_file):
-    print("extracting 100 most recent posts per author")
-    multi_core = False
-    reddits = pd.read_csv(data_file).set_index('idx')
-    map_reducer = MapReducer(reddits)
-    groups = reddits.groupby("author").groups.items()
-    results = []
-    if multi_core:
-        p = mp.Pool(mp.cpu_count())  # Data parallelism Object
-        results = p.map(map_reducer.mapper, groups)
+    recent_df_path = os.path.dirname(data_file) + "/recent100.pkl"
+    if os.path.isfile(recent_df_path):
+        return pd.read_pickle(recent_df_path)
     else:
-        for group in groups:
-            results.append(map_reducer.mapper(group))
-    x = pd.DataFrame(results, columns=['author', 'text']).set_index('author')
-    print("#done#")
-    return x
+        print("extracting 100 most recent posts per author")
+        multi_core = False
+        reddits = pd.read_csv(data_file).set_index('idx')
+        map_reducer = MapReducer(reddits)
+        groups = reddits.groupby("author").groups.items()
+        results = []
+        if multi_core:
+            p = mp.Pool(mp.cpu_count())  # Data parallelism Object
+            results = p.map(map_reducer.mapper, groups)
+        else:
+            for group in groups:
+                results.append(map_reducer.mapper(group))
+        x = pd.DataFrame(results, columns=['author', 'text']).set_index('author')
+        print("#done#")
+        pd.to_pickle(x, recent_df_path)
+        return x
 
 
 if __name__ == "__main__":
