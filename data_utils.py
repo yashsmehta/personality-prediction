3ae12c8427ee7a14457660bec4855e3a4688719b
Yash Mehta <yashsmehta95@gmail.com>
2020-06-08 23:54:24 +0100

    modified preprocessing func to include full stops and changed the naming of pkl files (embeddings)

--- data_utils.py
+++ data_utils.py
@@ -20,9 +20,9 @@ def preprocess_text(sentence):
     sentence = re.sub(r'http\S+', ' ', sentence)
     # Remove punctuations and numbers
     # sentence = re.sub('[^a-zA-Z]', ' ', sentence)
-    sentence = re.sub('[^a-zA-Z.?!,]', ' ', sentence)
+    # sentence = re.sub('[^a-zA-Z.?!,]', ' ', sentence)
     # Single character removal (except I)
-    sentence = re.sub(r"\s+[a-zA-HJ-Z]\s+", ' ', sentence)
+    # sentence = re.sub(r"\s+[a-zA-HJ-Z]\s+", ' ', sentence)
     # Removing multiple spaces
     sentence = re.sub(r'\s+', ' ', sentence)
 
