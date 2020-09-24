8bc265a50e0e0950ebfccc592ba39abefc3d85f6
amirmohammadkz <amirmohammadkz@gmail.com>
2020-09-24 15:15:10 +0330

    remove '|||' and add author_ids as int

--- utils/dataset_processors.py
+++ utils/dataset_processors.py
@@ -5,6 +5,7 @@ import csv
 import preprocessor as p
 import math
 
+
 def preprocess_text(sentence):
     # remove hyperlinks, hashtags, smileys, emojies
     sentence = p.clean(sentence)
@@ -17,6 +18,7 @@ def preprocess_text(sentence):
     # sentence = re.sub(r"\s+[a-zA-HJ-Z]\s+", ' ', sentence)
     # Removing multiple spaces
     sentence = re.sub(r'\s+', ' ', sentence)
+    sentence = re.sub(r'\|\|\|', ' ', sentence)
 
     return sentence
 
@@ -25,7 +27,7 @@ def load_essays_df(datafile):
     with open(datafile, "rt") as csvf:
         csvreader = csv.reader(csvf, delimiter=',', quotechar='"')
         first_line = True
-        df = pd.DataFrame(columns=["user", "text" , "token_len" , "EXT", "NEU", "AGR", "CON", "OPN"])
+        df = pd.DataFrame(columns=["user", "text", "token_len", "EXT", "NEU", "AGR", "CON", "OPN"])
         for line in csvreader:
             if first_line:
                 first_line = False
@@ -60,45 +62,50 @@ def essays_embeddings(datafile, tokenizer, token_length, mode):
     df = load_essays_df(datafile)
     cnt = 0
     num_subdocs = []
-    
-    #sorting all essays in ascending order of their length
+
+    # sorting all essays in ascending order of their length
     for ind in df.index:
         tokens = tokenizer.tokenize(df['text'][ind])
         df.at[ind, 'token_len'] = len(tokens)
-    
-    df.sort_values(by=['token_len', 'user'],inplace=True, ascending=True)
+
+    df.sort_values(by=['token_len', 'user'], inplace=True, ascending=True)
     print(df['token_len'])
     print(df['token_len'].mean())
 
     for ii in range(len(df)):
         text = preprocess_text(df['text'][ii])
         tokens = tokenizer.tokenize(text)
-    
+
         if mode == 'normal' or mode == '512_head':
-            input_ids.append(tokenizer.encode(tokens, add_special_tokens=True, max_length=token_length, pad_to_max_length=True))
+            input_ids.append(
+                tokenizer.encode(tokens, add_special_tokens=True, max_length=token_length, pad_to_max_length=True))
         elif mode == '512_tail':
-            input_ids.append(tokenizer.encode(tokens[-(token_length-2):], add_special_tokens=True, max_length=token_length, pad_to_max_length=True))
+            input_ids.append(
+                tokenizer.encode(tokens[-(token_length - 2):], add_special_tokens=True, max_length=token_length,
+                                 pad_to_max_length=True))
         elif mode == '256_head_tail':
-            input_ids.append(tokenizer.encode(tokens[:(token_length-1)]+tokens[-(token_length-1):], add_special_tokens=True, max_length=token_length, pad_to_max_length=True))
-        
+            input_ids.append(
+                tokenizer.encode(tokens[:(token_length - 1)] + tokens[-(token_length - 1):], add_special_tokens=True,
+                                 max_length=token_length, pad_to_max_length=True))
+
         elif mode == 'docbert':
             docmax_len = 2048
             subdoc_len = 512
             max_subdoc_num = docmax_len // subdoc_len
-            subdoc_tokens = [tokens[i:i+subdoc_len] for i in range(0, len(tokens), subdoc_len)][:max_subdoc_num]
+            subdoc_tokens = [tokens[i:i + subdoc_len] for i in range(0, len(tokens), subdoc_len)][:max_subdoc_num]
             # print(subdoc_tokens)
             token_ids = [tokenizer.encode(x, add_special_tokens=True, max_length=token_length, pad_to_max_length=True)
                          for x in subdoc_tokens]
             # print(token_ids)
             token_ids = np.array(token_ids).astype(int)
-            
-            buffer_len = docmax_len//subdoc_len - token_ids.shape[0]
+
+            buffer_len = docmax_len // subdoc_len - token_ids.shape[0]
             # print(buffer_len)
             tmp = np.full(shape=(buffer_len, token_length), fill_value=0, dtype=int)
             token_ids = np.concatenate((token_ids, tmp), axis=0)
-            
+
             input_ids.append(token_ids)
-        
+
         if (cnt < 3):
             print(input_ids[-1])
 
@@ -106,8 +113,9 @@ def essays_embeddings(datafile, tokenizer, token_length, mode):
         cnt += 1
 
     author_ids = np.array(df.index)
-    print('loaded all input_ids and targets from the data file!')    
+    print('loaded all input_ids and targets from the data file!')
     return author_ids, input_ids, targets
+
 
 def load_Kaggle_df(datafile):
     with open(datafile, "rt") as csvf:
@@ -156,13 +164,14 @@ def kaggle_embeddings(datafile, tokenizer, token_length):
 
         input_ids.append(token_ids)
         targets.append([df['E'][ind], df['N'][ind], df['F'][ind], df['J'][ind]])
-        author_ids.append(df['user'][ind])
+        author_ids.append(int(df['user'][ind]))
         cnt += 1
     print('token lengths : ', token_len)
     print('average length : ', int(np.mean(token_len)))
     author_ids = np.array(author_ids)
 
     return author_ids, input_ids, targets
+
 
 def load_pandora_df(datafile):
     # load posts_df in a proper [author,100 most recent posts] format
