798607370709251f16197fb13ae28fc8cba4023f
amirmohammadkz <amirmohammadkz@gmail.com>
2020-09-24 17:50:33 +0330

    Some bug fixes for order and kaggle preprocessing

--- finetune_models/MLP_combined_features.py
+++ finetune_models/MLP_combined_features.py
@@ -28,6 +28,7 @@ feature_flags = [nrc, nrc_vad, readability, mairesse]
 
 start = time.time()
 
+
 def merge_features(embedding, other_features, full_targets):
     df = pd.merge(embedding, other_features, left_index=True, right_index=True)
     df = pd.merge(df, full_targets, left_index=True, right_index=True)
@@ -35,6 +36,7 @@ def merge_features(embedding, other_features, full_targets):
     data_arr = df[df.columns[:-len(trait_labels)]].values
     targets_arr = df[df.columns[-len(trait_labels):]].values
     return data_arr, targets_arr
+
 
 if (re.search(r'base', embed)):
     n_hl = 12
@@ -76,7 +78,7 @@ print('inputs shape: ', np.array(inputs).shape)
 print('author_ids shape: ', np.array(author_ids).shape)
 
 inputs = pd.DataFrame(np.array(inputs))
-inputs['order'] = author_ids[0]
+inputs['order'] = author_ids
 inputs = inputs.set_index(['order'])
 full_targets = pd.DataFrame(np.array(targets))
 full_targets['order'] = author_ids[0]
@@ -85,10 +87,11 @@ full_targets = full_targets.set_index(['order'])
 if dataset == 'essays':
     # dump_data = pd.read_csv('data/essays/essays.csv', index_col='#AUTHID')
     dump_data = dataset_processors.load_essays_df('data/essays/essays.csv')
-    trait_labels = ['EXT','NEU','AGR','CON','OPN']
+    trait_labels = ['EXT', 'NEU', 'AGR', 'CON', 'OPN']
 
 elif dataset == 'kaggle':
-    dump_data = pd.read_csv('data/kaggle/kaggle.csv', index_col='id')
+    # dump_data = pd.read_csv('data/kaggle/kaggle.csv', index_col='id')
+    dump_data = dataset_processors.load_Kaggle_df('data/kaggle/kaggle.csv')
     trait_labels = ['E', 'N', 'F', 'J']
 
 other_features_df = feature_utils.get_psycholinguist_data(dump_data, dataset, feature_flags)
@@ -97,22 +100,22 @@ inputs, full_targets = merge_features(inputs, other_features_df, full_targets)
 n_splits = 10
 fold_acc = {}
 expdata = {}
-expdata['acc'], expdata['trait'], expdata['fold'] = [],[],[]
+expdata['acc'], expdata['trait'], expdata['fold'] = [], [], []
 
 for trait_idx in range(full_targets.shape[1]):
     # convert targets to one-hot encoding
     targets = full_targets[:, trait_idx]
     n_data = targets.shape[0]
-    
+
     expdata['trait'].extend([trait_labels[trait_idx]] * n_splits)
-    expdata['fold'].extend(np.arange(1,n_splits+1))
+    expdata['fold'].extend(np.arange(1, n_splits + 1))
 
     skf = StratifiedKFold(n_splits=n_splits, shuffle=False)
     k = -1
     for train_index, test_index in skf.split(inputs, targets):
         x_train, x_test = inputs[train_index], inputs[test_index]
         y_train, y_test = targets[train_index], targets[test_index]
-        #converting to one-hot embedding
+        # converting to one-hot embedding
         y_train = tf.keras.utils.to_categorical(y_train, num_classes=n_classes)
         y_test = tf.keras.utils.to_categorical(y_test, num_classes=n_classes)
         model = tf.keras.models.Sequential()
@@ -122,37 +125,38 @@ for trait_idx in range(full_targets.shape[1]):
         # model.add(tf.keras.layers.Dense(50, activation='relu'))
         model.add(tf.keras.layers.Dense(n_classes))
 
-        k+=1
+        k += 1
         model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
-                loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                metrics=['mse', 'accuracy'])
-        
+                      loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                      metrics=['mse', 'accuracy'])
+
         history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size,
                             validation_data=(x_test, y_test), verbose=0)
-        
+
         # if(k==0):
         #     print(model.summary())
-        
+
         # print('\nacc: ', history.history['accuracy'])
         print('val acc: ', history.history['val_accuracy'])
         # print('loss: ', history.history['loss'])
         # print('val loss: ', history.history['val_loss'])
         expdata['acc'].append(100 * max(history.history['val_accuracy']))
 
-print (expdata)
+print(expdata)
 
 df = pd.DataFrame.from_dict(expdata)
 
-df['network'], df['dataset'], df['lr'], df['batch_size'], df['epochs'], df['embed'], df['layer'], df['mode'], df['embed_mode'], df['jobid'] = network,  \
-                                                                    dataset, lr, batch_size, epochs, embed, layer, mode, embed_mode, jobid
+df['network'], df['dataset'], df['lr'], df['batch_size'], df['epochs'], df['embed'], df['layer'], df['mode'], df[
+    'embed_mode'], df['jobid'] = network, \
+                                 dataset, lr, batch_size, epochs, embed, layer, mode, embed_mode, jobid
 
 pd.set_option('display.max_columns', None)
 print(df.head(5))
 
 # save the results of our experiment
-if(log_expdata):
+if (log_expdata):
     Path(path).mkdir(parents=True, exist_ok=True)
-    if(not os.path.exists(path + 'expdata.csv')):
+    if (not os.path.exists(path + 'expdata.csv')):
         df.to_csv(path + 'expdata.csv', mode='a', header=True)
     else:
         df.to_csv(path + 'expdata.csv', mode='a', header=False)
